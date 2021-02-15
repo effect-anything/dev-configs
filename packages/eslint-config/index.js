fd28de6b6e6f54230db68f2c321ad7d9edcf4fdd
Kee <121441909zk@gmail.com>
2021-02-15 15:56:56 +0800

    update babel options plugins

--- packages/eslint-config/index.js
+++ packages/eslint-config/index.js
@@ -9,7 +9,7 @@ module.exports = {
     sourceType: "module",
     requireConfigFile: false,
     babelOptions: {
-      presets: ["@babel/preset-react"],
+      plugins: ["@babel/plugin-syntax-jsx"],
     },
   },
   plugins: ["prettier"],
